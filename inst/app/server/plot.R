3dbb074312123a790179b3f8ca3e50e7a3653dee
Jack Olney <jack.olney@me.com>
2017-03-01 14:57:35 +0000

    tooltip tweaks

--- inst/app/server/plot.R
+++ inst/app/server/plot.R
@@ -11,9 +11,14 @@ output$plot <- renderHighchart({
     # subset if not all checkboxes ticked.
     out <- out[out$variable %in% input$selected_initial,]
 
+    # highcharter thousandsSep
+    hcopts <- getOption("highcharter.lang")
+    hcopts$thousandsSep <- ","
+    options(highcharter.lang = hcopts)
+
     # highcharter
     hchart(object = out, type = "line", hcaes(x = t, y = value, group = variable)) %>%
-        hc_tooltip(crosshairs = TRUE, shared = TRUE, borderWidth = 2) %>%
+        hc_tooltip(crosshairs = TRUE, shared = TRUE, borderWidth = 2, valueDecimals = 2) %>%
         hc_exporting(enabled = TRUE) %>%
         hc_chart(zoomType = "x")
 })
