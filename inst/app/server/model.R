e6bd8850080a5ec53026404ce37143edc3d41723
Jack Olney <jack.olney@me.com>
2017-03-06 17:06:11 +0000

    dynamic parameter updates that are listened to by odin

--- inst/app/server/model.R
+++ inst/app/server/model.R
@@ -23,34 +23,25 @@ check_parameters <- function(model) {
     # empty changes vector
     changes <- vector()
 
-    # require the following
-    # req(input$param_mu, input$param_mu)
+    # list all parameters
+    vals$params
 
-    # mu
-    if ("mu" %in% input$selected_parameters & !is.null(input$param_mu)) {
-        if (input$param_mu != base[["mu"]]) {
-            changes <- c(changes, mu = input$param_mu)
-        }
-    }
+    # which sliders are active?
+    params <- vals$params[vals$params %in% input$selected_parameters]
 
-    # beta
-    if ("beta" %in% input$selected_parameters & !is.null(input$param_beta)) {
-        if (input$param_beta != base[["beta"]]) {
-            changes <- c(changes, beta = input$param_beta)
-        }
-    }
-
-    # sigma
-    if ("sigma" %in% input$selected_parameters & !is.null(input$param_sigma)) {
-        if (input$param_sigma != base[["sigma"]]) {
-            changes <- c(changes, sigma = input$param_sigma)
-        }
-    }
-
-    # delta
-    if ("delta" %in% input$selected_parameters & !is.null(input$param_delta)) {
-        if (input$param_delta != base[["delta"]]) {
-            changes <- c(changes, delta = input$param_delta)
+    # now we need to loop through them and add to our changes vector
+    for (i in 1:length(params)) {
+        # assign parameter name
+        param_name <- paste0("param_", params[i])
+        # compare against model$contents(), if different, then assign change
+        if (!is.null(input[[param_name]])) {
+            if (input[[param_name]] != base[[params[i]]]) {
+                changes <- c(changes, input[[param_name]])
+                if (is.null(names(changes)))
+                    names(changes) <- params[i]
+                else
+                    names(changes)[which(names(changes) == "")] <- params[i]
+            }
         }
     }
 
@@ -61,13 +52,8 @@ run_model <- function(model) {
 
     # parameter updates
     replace <- check_parameters(model)
-
-    editable <- c("mu", "beta", "sigma", "delta")
-    if (length(replace) > 0L) {
-        stopifnot(is.numeric(replace))
-        stopifnot(all(names(replace) %in% editable))
+    if (length(replace) > 0L)
         model$set_user(user = as.list(replace))
-    }
 
     # run the model
     tt <- seq(from = input$time_from, to = input$time_to, by = input$time_by)
