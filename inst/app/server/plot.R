6b5d8958b2a1f4f0b0eac09343daf569a920b69b
Jack Olney <jack.olney@me.com>
2017-03-07 14:29:28 +0000

    custom theme

--- inst/app/server/plot.R
+++ inst/app/server/plot.R
@@ -23,13 +23,38 @@ output$plot <- renderHighchart({
     # highcharter
     hchart(object = out, type = "line", hcaes(x = t, y = value, group = variable)) %>%
         hc_tooltip(crosshairs = TRUE, shared = TRUE, borderWidth = 1.5, valueDecimals = 2,
-                   style = list(fontSize = "12px")) %>%
-        hc_exporting(enabled = TRUE) %>%
+            style = list(fontSize = "12px")) %>%
+        hc_exporting(enabled = TRUE, filename = "modelr-output",
+            buttons = list(contextButton = list(align = "right", x = -10, y = 1))) %>%
         hc_chart(zoomType = "x",
-                 resetZoomButton = list(position = list(align = "right", x = -30))) %>%
+            resetZoomButton = list(position = list(align = "right", x = -40))) %>%
         hc_xAxis(title = list(text = "time"),
-                 labels = list(style = list(fontSize = "12px"))) %>%
+            labels = list(style = list(fontSize = "12px"))) %>%
         hc_yAxis(title = list(text = "value"),
-                labels = list(style = list(fontSize = "12px"))) %>%
-        hc_legend(x = 22, itemStyle = list(fontSize = "15px"))
+            labels = list(style = list(fontSize = "12px"))) %>%
+        hc_legend(itemStyle = list(fontSize = "15px")) %>%
+        hc_add_theme(hc_theme_modelr)
 })
+
+# custom highcharter theme (modified from hc_theme_smpl, with colors from hc_theme_538)
+hc_theme_modelr <- hc_theme(
+    colors = c("#FF2700", "#008FD5", "#77AB43", "#636464", "#C4C4C4"),
+    chart = list(style = list(fontFamily = "Roboto")),
+    title = list(align = "left", style = list(fontFamily = "Roboto Condensed", fontWeight = "bold")),
+    subtitle = list(align = "left", style = list(fontFamily = "Roboto Condensed")),
+    legend = list(align = "center", verticalAlign = "bottom"),
+    xAxis = list(gridLineWidth = 1, gridLineColor = "#D7D7D8",
+        labels = list(style = list(fontFamily = "Roboto",
+            color = "#3C3C3C")), lineColor = "#D7D7D8", minorGridLineColor = "#505053",
+        tickColor = "#D7D7D8", tickWidth = 1, title = list(style = list(color = "#A0A0A3"))),
+    yAxis = list(gridLineColor = "#D7D7D8", labels = list(style = list(fontFamily = "Roboto",
+        color = "#3C3C3C")), lineColor = "#D7D7D8", minorGridLineColor = "#505053",
+        tickColor = "#D7D7D8", tickWidth = 1, title = list(style = list(color = "#A0A0A3"))),
+    labels = list(style = list(color = "#D7D7D8")),
+    plotOptions = list(line = list(marker = list(enabled = FALSE),
+        states = list(hover = list(lineWidthPlus = 1))),
+        spline = list(marker = list(enabled = FALSE), states = list(hover = list(lineWidthPlus = 1))),
+        area = list(marker = list(enabled = FALSE), states = list(hover = list(lineWidthPlus = 1))),
+        areaspline = list(marker = list(enabled = FALSE),
+            states = list(hover = list(lineWidthPlus = 1))))
+)
