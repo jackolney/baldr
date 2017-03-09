05bf96eea10d64300d186d90990770836c7d2306
Jack Olney <jack.olney@me.com>
2017-03-09 17:07:54 +0000

    restore test

--- tests/testthat/test-model.R
+++ tests/testthat/test-model.R
@@ -5,7 +5,7 @@ test_that("Compare Models", {
     odin <- sir_model_odin()
     r <- sir_model_r()
     # compare output
-    expect_equal(1, 1,
+    expect_equal(r, odin,
         check.attributes = FALSE,
         tolerance = 0.01,
         info = "Odin and R do not produce the same result")
