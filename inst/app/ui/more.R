096e0f27232cf90d0d743fdc596f47999b1105d0
Jack Olney <jack.olney@me.com>
2017-03-13 11:54:48 +0000

    iframe to manual in app

--- inst/app/ui/more.R
+++ inst/app/ui/more.R
@@ -1,9 +1,8 @@
 navbarMenu("More",
+    tabPanel("Manual",
+        HTML('<iframe src=\"https://jackolney.github.io/modelr/manual\"style=\"border: 0; position:absolute; top:50px; left:0; right:0; width:100%; height:100%\"></iframe>')
+    ),
     tabPanel("Diagnostics",
         verbatimTextOutput(outputId = "diagnostics")
-    ),
-    tabPanel("About",
-        "Jack made this.",
-        a(href = "https://jackolney.github.io/modelr/manual", "modelr manual", target = "_blank")
     )
 )
